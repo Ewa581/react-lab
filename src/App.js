490b8cff303deb756da03b80db7527435c0ccc92
Mosh <programm@gmail.com>
2025-01-12 14:29:23 +0100

    Interakcje

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import {useState} from "react";
 
 function App() {
 
-  const [title, setTitle] = useState('Wall-E')
+  const [title, setTitle] = useState('Wall-E');
 
+  let message;
+  if(title.length < 5) {
+    message = "Tytuł jest za krótki"  
+  } else if (title.length < 15) {
+    message = "Tytuł jest w sam raz"
+  } else {
+    message = "Tytuł jest za długi"
+  }
   function handleChange(event) {
     setTitle(event.target.value);
   }  
@@ -13,6 +21,7 @@ function App() {
       <div>
           <h1>My favourite movies to watch</h1>
           <h2>My favourite movie for today is {title}</h2>
+          {title.length > 0 && <div> {message}</div>}
           <input type="text" onChange={handleChange}/>
       </div>
   );
